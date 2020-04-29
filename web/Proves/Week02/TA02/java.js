5db1434d4561e36b9731dbb13b3819a14ade5e8d
jordan-hamblin <64113676+jordan-hamblin@users.noreply.github.com>
2020-04-29 14:12:49 -0600

    Fixing filepath

---
+++ web/Proves/Week02/TA02/java.js
@@ -0,0 +1,12 @@
+ 
+
+function clicked() {
+  alert("I'm not really sure why this exists");
+ }
+
+ function change() {
+  let txtColor = document.getElementById("txtColor").value;
+  document.getElementById("div1").style.color = txtColor;
+}
+
+
