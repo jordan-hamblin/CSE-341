5b57ad89b8770d90beda5e63bbdcff9a1ba4e0ec
jordan-hamblin <64113676+jordan-hamblin@users.noreply.github.com>
2020-05-28 17:09:44 -0600

    testing database

--- web/Proves/Week05/java.js
+++ web/Proves/Week05/java.js
@@ -5,7 +5,8 @@ window.onload = function(){
     document.getElementById("action").addEventListener('onchange', show);
 }
 
-function show(){
+function show(event){
+    console.log(event);
     let action = document.getElementById("action").value;
     if (action == "insert"){
             document.getElementById("content").style.display = "block";
