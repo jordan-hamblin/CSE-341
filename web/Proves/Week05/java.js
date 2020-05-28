b79ad4cd7e521e76ab011b1ed29540b6432766b4
jordan-hamblin <64113676+jordan-hamblin@users.noreply.github.com>
2020-05-28 17:17:59 -0600

    testing database

--- web/Proves/Week05/java.js
+++ web/Proves/Week05/java.js
@@ -10,5 +10,8 @@ function show(event){
     let action = document.getElementById("action").value;
     if (action == "insert"){
             document.getElementById("content").style.display = "block";
+    }
+    else{
+        document.getElementById("content").style.display = "none";
     }   
 }
