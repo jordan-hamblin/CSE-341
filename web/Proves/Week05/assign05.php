20b6279d56b4d79fe1c338b9b1c39232a3783519
jordan-hamblin <64113676+jordan-hamblin@users.noreply.github.com>
2020-05-28 17:00:50 -0600

    testing database

--- web/Proves/Week05/assign05.php
+++ web/Proves/Week05/assign05.php
@@ -19,7 +19,9 @@
             <option value="insert">Insert</option>
         </select>
         
-        
+        <div id="content">
+            <textarea id="document_content" name="document_content">Document Content:</textarea>
+        </div>
 
         <input type="submit" value="Perform requested action" name="show_document">
 	</form>
